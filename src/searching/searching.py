506dd42108389c73a9185001cbda70c621b6eaa4
michhottinger <michhottinger@users.noreply.github.com>
2020-06-17 09:43:02 -0700

    fixed my code for merge

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,26 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
-
+    if start <= end: #check base:target is midpoint
+                    #or range of search space
+    
+        mid = (start + end) // 2
+    
+    #check if target is the actual mid
+        if arr[mid] == target:
+            return mid
+    
+    #check if target is smaller than mid
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid -1)
+    
+    #check if target is larger than mid
+        else:
+            return binary_search(arr, target, end, mid + 1)
+    
+    #target is not in array return down to end
+    else:
+        return -1
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -10,5 +29,8 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
+    
+    #base will be random
+    if arr[0]
     # Your code here
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,19 +1,57 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    merged_arr = [0] * elements #creates a list of 0's 
+    #[0 for _ in range(elements)] is another way to do this
 
     # Your code here
-       while a < len(A):
-            combined.append(A[a])
-            a += 1
-        while b < len(B):
-            combined.append(B[b])
+    #start pointers at the start of both lists
+    #compare value of pointers and smallest gets added to merge list
+    #increment pointer to move onto next list
+    
+    #initialize each list for both pointers
+    a = 0
+    b = 0
+    
+    #loop through merged_arr
+    for i in range(len(merged_arr)):
+        #make sure pointer is in range of array
+        #if it is out of range, copy other array
+        if a >= len(arrA):
+            merged_arr[i] = arrB[b]
             b += 1
-            
-        return combined
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a += 1
+        elif arrA[a] < arrB[b]: #both indices are in range of their own array
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
 
+            
     return merged_arr
+
+#     while a < len(arrA) and b < len(arrB):
+#         if arrA[a] < arrB[b]:
+#             combined.append(arrA[a])
+#             a += 1
+#         else:
+#             combined.append(arrB[b])
+#             b +=1
+            
+#         #this will create the combined list
+    
+#         while a < len(arrA):
+#              combined.append(arrA[a])
+#              a += 1
+#         while b < len(arrB):
+#              combined.append(arrB[b])
+#              b += 1
+            
+#         return combined
+
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
@@ -25,14 +63,15 @@ def merge_sort(arr):
 
     return arr
 
+
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
+#     pass
 
-
-def merge_sort_in_place(arr, l, r):
-    # Your code here
-
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
+#     pass
